342057a7222f4b91ea96bb335a560d2eb9741d52
Oren Leiman <mumblemumble777@gmail.com>
2017-08-08 15:40:29 -0700

    Comments IK_Server and remove cruft. Increase delay in trajectory sampler to ensure that the target object is successfully gripped

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -17,7 +17,8 @@ from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from geometry_msgs.msg import Pose
 from mpmath import *
 from sympy import *
-
+import matplotlib.pyplot as plt
+import atexit
 
 def handle_calculate_IK(req):
     rospy.loginfo("Received %s eef-poses from the plan" % len(req.poses))
@@ -97,33 +98,6 @@ def handle_calculate_IK(req):
         T0_6 = T0_5 * T5_6
         T0_G = T0_6 * T6_G
         
-        # Rotate gripper reference frame to coincide with global reference frame
-        
-        R_z_180 = Matrix([[cos(pi), -sin(pi), 0, 0],
-                          [sin(pi), cos(pi),  0, 0],
-                          [0,       0,        1, 0],
-                          [0,       0,        0, 1]])
-        
-        R_y_neg90 = Matrix([[cos(-pi/2),  0,  sin(-pi/2), 0],
-                            [0,           1,  0,          0],
-                            [-sin(-pi/2), 0,  cos(-pi/2), 0],
-                            [0,           0,  0,          1]])
-        
-        # correction rotation from DH to URDF
-        # R_corr = simplify(R_z_180 * R_y_neg90)
-        
-        # for debugging
-        # q_subs = {q1: 0.51, q2: -0.61, q3: -2.13, q4: -2.90, q5: -1.49, q6: 0.56}
-        # print("T0_1 = ", T0_1.evalf(subs=q_subs))
-        # print("T0_2 = ", T0_2.evalf(subs=q_subs))
-        # print("T0_3 = ", T0_3.evalf(subs=q_subs))
-        # print("T0_4 = ", T0_4.evalf(subs=q_subs))
-        # print("T0_5 = ", T0_5.evalf(subs=q_subs))
-        # print("T0_6 = ", T0_6.evalf(subs=q_subs))
-        # print("T0_G = ", T0_G.evalf(subs=q_subs))
-        
-        # T_total = T0_G * R_corr
-        
         # Extract rotation matrices from the transformation matrices
         # The rotation matrix is defined as the 3x3 submatrix beginning
         # at (0, 0) for each homogeneous transform.
@@ -140,8 +114,10 @@ def handle_calculate_IK(req):
         
         ## Insert IK code here!
         joint_trajectory_list = []
+        end_effector_errors = []
         for x in range(0, len(req.poses)):
             joint_trajectory_point = JointTrajectoryPoint()
+            
             # Extract end-effector position and orientation from request
             # px,py,pz = end-effector position
             # roll, pitch, yaw = end-effector orientation
@@ -149,14 +125,14 @@ def handle_calculate_IK(req):
             py = req.poses[x].position.y
             pz = req.poses[x].position.z
             
-            
             (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
                 [req.poses[x].orientation.x, req.poses[x].orientation.y,
                  req.poses[x].orientation.z, req.poses[x].orientation.w])
             
             ### Your IK code here 
-            # Compensate for rotation discrepancy between DH parameters and Gazebo
-            
+
+            # elementary rotations corresponding to yaw (Z), pitch (Y), and roll (X)
+            # Rotation matrix is derived using the ZYX convention
             R_yaw = Matrix([[cos(yaw), -sin(yaw), 0],
                             [sin(yaw), cos(yaw), 0],
                             [0, 0, 1]])
@@ -174,19 +150,20 @@ def handle_calculate_IK(req):
             R_z_180 = Matrix([[cos(pi), -sin(pi), 0],
                               [sin(pi), cos(pi),  0],
                               [0,       0,        1]])
-            
-            
-            R_URDF_to_DH = R_y_90 * R_z_180
 
+            # Rotate incoming orientation data into coincidence with EE frame
+            R_URDF_to_DH = R_y_90 * R_z_180
             Rrpy = (R_yaw * R_pitch * R_roll* R_URDF_to_DH).evalf()
-    
+
+            # orientation of the Z-axis for the end effector frame
             nx = Rrpy[0,2]
             ny = Rrpy[1,2]
             nz = Rrpy[2,2]
-            
+
+            # calculate wrist center position as a translation of magnitude d7
+            # along the Z-axis of the end effector frame as calculated above
             wx = (px - d7 * nx)
             wx = wx.subs(s)
-            
             
             wy = py - d7 * ny
             wy = wy.subs(s)
@@ -195,55 +172,71 @@ def handle_calculate_IK(req):
             wz = wz.subs(s)
             
             # Calculate joint angles using Geometric IK method
-            #
-            #
-            ###
             
             # triangle sides
             A = a2
             B = sqrt(a3**2 + d4**2)
             C = sqrt((sqrt(wx**2 + wy**2)-a1)**2 + (wz - d1)**2)
-            
-            theta1 = atan2(wy, wx).evalf(subs=s)
-            
+
+            # apply law of cosines (more detail in writeup)
             cos_gamma = (A**2 + C**2 - B**2) / (2 * A * C)
             
             gamma = atan2(sqrt(1-cos_gamma**2), cos_gamma)
             
-            theta2 = (pi/2 - gamma - atan2(wz-d1, sqrt(wx**2 + wy**2) - a1)).subs(s)
-            
             cos_beta = (A**2 + B**2 - C**2) / (2 * A * B)
             
             beta = atan2(sqrt(1-cos_beta**2), cos_beta)
-            
-            
+
+            # calculate theta 1-3 using angles obtained above
+            # see diagram in writeup for more detail.
+            theta1 = atan2(wy, wx).evalf(subs=s)
+            theta2 = (pi/2 - gamma - atan2(wz-d1, sqrt(wx**2 + wy**2) - a1)).subs(s)            
             theta3 = (pi/2 - beta - atan2(abs(a3), d4)).subs(s)
-            
+
+            # obtain rotation matrix from joint 3 to joint 6, which by definition has the
+            # same orientation as the end effector, which is rigidly joined to joint 6
             R0_3_inv = Transpose(R0_3.evalf(subs={q1: theta1, q2: theta2, q3: theta3}))
             R3_6 = (R0_3_inv * Rrpy).evalf()
 
             # print symbolic representation of R3_6, derive the rotation order from that
             # pprint(simplify(T3_4[0:3,0:3] * T4_5[0:3,0:3] * T5_6[0:3,0:3]))
-            
+
+            # final 3 thetas calculated from Euler angle decomposition of
+            # rotation 3->6
             theta4 = atan2(R3_6[2,2], -R3_6[0,2])
             theta5 = atan2(sqrt(R3_6[1,0]**2 + R3_6[1,1]**2), R3_6[1,2])
             theta6 = atan2(-R3_6[1,1], R3_6[1,0])
+
+            # calculate forward kinematics to output
+            q_subs = {q1: theta1, q2: theta2, q3: theta3, q4: theta4, q5: theta5, q6: theta6}
+            FK = T0_G.evalf(subs=q_subs)
+            my_ee = FK[0:3, 3]
+            ee_x_e = abs(my_ee[0] - px)
+            ee_y_e = abs(my_ee[1] - py)
+            ee_z_e = abs(my_ee[2] - pz)
+            ee_offset = sqrt(ee_x_e**2 + ee_y_e**2 + ee_z_e**2)
+            end_effector_errors.append(ee_offset)
 	    
             # Populate response for the IK request
-            # In the next line replace theta1,theta2...,theta6 by your joint angle variables
 	    joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
 	    joint_trajectory_list.append(joint_trajectory_point)
         
     rospy.loginfo("length of Joint Trajectory List: %s" % len(joint_trajectory_list))
+    plt.plot(end_effector_errors)
+    plt.ylabel('EE position error')
+    plt.savefig('effector_error_plot.png')
     return CalculateIKResponse(joint_trajectory_list)
     
-    
 def IK_server():
+    request_idx = 0
     # initialize node and declare calculate_ik service
     rospy.init_node('IK_server')
     s = rospy.Service('calculate_ik', CalculateIK, handle_calculate_IK)
     print "Ready to receive an IK request"
     rospy.spin()
+
+def save_figure():
+    plt.savefig('effector_error_plot.png')
 
 if __name__ == "__main__":
     IK_server()
